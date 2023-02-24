27daa8d748e23b7847beb2760e2ccb0df17c5dae
akferreira <49925514+akferreira@users.noreply.github.com>
2023-02-23 21:28:37 -0300

    Versão inicial das funções auxiliares

--- kit_optimization/eight_queens.py
+++ kit_optimization/eight_queens.py
@@ -1,53 +1,54 @@
-def evaluate(individual):
-    """
-    Recebe um indivíduo (lista de inteiros) e retorna o número de ataques
-    entre rainhas na configuração especificada pelo indivíduo.
-    Por exemplo, no individuo [2,2,4,8,1,6,3,4], o número de ataques é 10.
+import random
+random.seed()
 
-    :param individual:list
-    :return:int numero de ataques entre rainhas no individuo recebido
-    """
-    raise NotImplementedError  # substituir pelo seu codigo
+def select_elite(participantes, elite_tamanho):
+  participantes.sort(key=evaluate)
+  return participantes[0:elite_tamanho]
 
-
-def tournament(participants):
-    """
-    Recebe uma lista com vários indivíduos e retorna o melhor deles, com relação
-    ao numero de conflitos
-    :param participants:list - lista de individuos
-    :return:list melhor individuo da lista recebida
-    """
-    raise NotImplementedError  # substituir pelo seu codigo
+def crossover(parent1,parent2, index):
+  return [parent1[0:index]+parent2[index:], parent2[0:index]+parent1[index:]]
 
 
-def crossover(parent1, parent2, index):
-    """
-    Realiza o crossover de um ponto: recebe dois indivíduos e o ponto de
-    cruzamento (indice) a partir do qual os genes serão trocados. Retorna os
-    dois indivíduos com o material genético trocado.
-    Por exemplo, a chamada: crossover([2,4,7,4,8,5,5,2], [3,2,7,5,2,4,1,1], 3)
-    deve retornar [2,4,7,5,2,4,1,1], [3,2,7,4,8,5,5,2].
-    A ordem dos dois indivíduos retornados não é importante
-    (o retorno [3,2,7,4,8,5,5,2], [2,4,7,5,2,4,1,1] também está correto).
-    :param parent1:list
-    :param parent2:list
-    :param index:int
-    :return:list,list
-    """
-    raise NotImplementedError  # substituir pelo seu codigo
+def mutate(individual,m):
+  if(random.random() < m):
+    index = random.randint(0,7)
+    new_pos = random.randint(1,8)
+    individual[index] = new_pos
+
+  return individual
 
 
-def mutate(individual, m):
-    """
-    Recebe um indivíduo e a probabilidade de mutação (m).
-    Caso random() < m, sorteia uma posição aleatória do indivíduo e
-    coloca nela um número aleatório entre 1 e 8 (inclusive).
-    :param individual:list
-    :param m:int - probabilidade de mutacao
-    :return:list - individuo apos mutacao (ou intacto, caso a prob. de mutacao nao seja satisfeita)
-    """
-    raise NotImplementedError  # substituir pelo seu codigo
+def tournament(participantes):
+  best_participante = None
+  best_aptitude = 99
 
+  for index,participante in enumerate(participantes):
+    current_aptitude = evaluate(participante)
+    if(current_aptitude < best_aptitude):
+      best_aptitude = current_aptitude
+      best_participante = participante
+
+  return best_participante
+
+
+
+def evaluate (positions):
+  attacks = 0
+
+  for column,line in enumerate(positions):
+    x1 = column
+    y1 = line
+
+    for column2,line2 in enumerate(positions[column+1:]):
+      x2 = column + column2 + 1
+      y2 = line2
+      if(y1 == y2):
+        attacks += 1
+
+      elif(abs(y2-y1) == abs(x2-x1)):
+        attacks += 1
+
+  return attacks
 
 def run_ga(g, n, k, m, e):
     """
