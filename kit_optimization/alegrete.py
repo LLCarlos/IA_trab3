6be8809df38da8b99eb556bf594eade3ef3bd1ef
Leonardo Carlos <leonardo_luis_carlos@dell.com>
2023-03-08 16:04:46 -0300

    alegretefix

--- kit_optimization/alegrete.py
+++ kit_optimization/alegrete.py
@@ -1,15 +1,7 @@
 import numpy as np
 
 
-def compute_mse(theta_0, theta_1, data):
-    """
-    Calcula o erro quadratico medio
-    :param theta_0: float - intercepto da reta
-    :param theta_1: float -inclinacao da reta
-    :param data: np.array - matriz com o conjunto de dados, x na coluna 0 e y na coluna 1
-    :return: float - o erro quadratico medio
-    """
-
+def compute_mse(theta_0, theta_1, data)
     mse = 0.0
     size = len(data)
     for d in data:
@@ -22,17 +14,8 @@ def compute_mse(theta_0, theta_1, data):
 
 
 def step_gradient(theta_0, theta_1, data, alpha):
-    """
-    Executa uma atualização por descida do gradiente  e retorna os valores atualizados de theta_0 e theta_1.
-    :param theta_0: float - intercepto da reta
-    :param theta_1: float -inclinacao da reta
-    :param data: np.array - matriz com o conjunto de dados, x na coluna 0 e y na coluna 1
-    :param alpha: float - taxa de aprendizado (a.k.a. tamanho do passo)
-    :return: float,float - os novos valores de theta_0 e theta_1, respectivamente
-    """
-
-    df_d0 = 0
-    df_d1 = 0
+    df_d0 = 0.0
+    df_d1 = 0.0
 
     for d in data:
         h0 = theta_0 + (d[0]*theta_1)
@@ -49,21 +32,6 @@ def step_gradient(theta_0, theta_1, data, alpha):
 
 
 def fit(data, theta_0, theta_1, alpha, num_iterations):
-    """
-    Para cada época/iteração, executa uma atualização por descida de
-    gradiente e registra os valores atualizados de theta_0 e theta_1.
-    Ao final, retorna duas listas, uma com os theta_0 e outra com os theta_1
-    obtidos ao longo da execução (o último valor das listas deve
-    corresponder à última época/iteração).
-
-    :param data: np.array - matriz com o conjunto de dados, x na coluna 0 e y na coluna 1
-    :param theta_0: float - intercepto da reta
-    :param theta_1: float -inclinacao da reta
-    :param alpha: float - taxa de aprendizado (a.k.a. tamanho do passo)
-    :param num_iterations: int - numero de épocas/iterações para executar a descida de gradiente
-    :return: list,list - uma lista com os theta_0 e outra com os theta_1 obtidos ao longo da execução
-    """
-
     thetas_0 = []
     thetas_1 = []
 
